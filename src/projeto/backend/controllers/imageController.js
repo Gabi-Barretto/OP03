8fa7bff774f6f4e650b526bc4227f67830348e46
naoassisto <99260189+naoassisto@users.noreply.github.com>
2024-12-06 09:15:07 -0300

    MONKEYY STYLEE

--- src/projeto/backend/controllers/imageController.js
+++ src/projeto/backend/controllers/imageController.js
@@ -1,42 +1,134 @@
-const { fetchImagesFromS3, uploadToSupabaseS3 } = require('../services/s3Service');
-const { createProgressBar } = require('../utils/progressUtil');
+// controllers/imageController.js
+// Full code with hardcoded image fetching, using the provided URLs, no omission
+
 const path = require('path');
 const fs = require('fs');
-
+const { PutObjectCommand } = require('@aws-sdk/client-s3');
+const s3Client = require('../config/s3Config');
+const { getFallbackMimeType } = require('../utils/mimeUtil');
+const { createProgressBar } = require('../utils/progressUtil');
 
 console.log(`⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠲⠲⠦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠞⠁⢤⣸⡄⠀⠆⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠋⠀⠙⠊⠀⠙⢢⡤⠄⡄⢱⡄⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠃⠀⠀⠀⢀⣀⣠⣤⣿⣤⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣉⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠏⠛⠈⠛⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⢀⣤⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠱⣤⣀⠀⠀⠀⣠⣻⠏⠀⠀⠀⠀⢻⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠙⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⠖⢉⡓⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡇⠀⠀⠀⠀⠀⣀⣀⡠⢤⣤⢄⠀⠀⡴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⣩⡴⠟⠛⠻⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣴⣶⣽⠟⠿⠉⠋⠉⠀⠻⣷⡴⠁⡜⠉⠛⢿⣿⣿⠿⠛⢋⣩⡴⠾⠛⠉⠀⠀⠀⠀⠈⠑⠦⡀⠀⠀⠀⠀⠀⠀⠀
-    ⠀⠀⠀⠀⠀⠀⣀⣠⡊⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣴⣽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠜⠀⢀⡠⠔⠋⢀⣠⠶⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣉⣀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠏⠛⠈⠛⠛⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠉⠀⠀⠀⠀⠀⢀⣤⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠱⣤⣀⠀⠀⠀⣠⣻⠏⠀⠀⠀⠀⢻⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠙⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⠖⢉⡓⢤⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡇⠀⠀⠀⠀⠀⣀⣀⡠⢤⣤⢄⠀⠀⡴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⣩⡴⠟⠛⠻⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⣴⣶⣽⠟⠿⠉⠋⠉⠀⠻⣷⡴⠁⡜⠉⠛⢿⣿⣿⠿⠛⢋⣩⡴⠾⠛⠉⠀⠀⠀⠀⠈⠑⠦⡀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⣀⣠⡊⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣴⣽⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠜⠀⢀⡠⠔⠋⢀⣠⠶⠋⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠳⣄⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⣰⠋⢿⡀⠀⣏⢿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⢈⢄⠀⠀⠀⠀⠀⠀⠀⣀⡠⢒⣉⠴⠂⠉⣀⡴⠞⢻⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠣⡀⠀⠀⠀
     ⠀⠀⠀⠀⣰⠁⠈⢾⣧⡀⣘⣄⠈⢯⡉⢉⣿⠋⠉⠀⠀⠀⠀⠁⠉⠖⠢⠄⠔⢒⡩⠵⠂⠉⣀⣤⠶⠋⠁⠀⠀⠘⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡀⠀⠀
     ⠀⠀⠀⠀⢧⣤⣤⡤⠛⢉⢄⡮⣦⣄⠈⠙⠣⢄⣠⠞⠉⠉⢩⢩⡷⠤⠤⠒⠊⠁⣠⣤⠶⠛⠉⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢱⡀⠀
     ⠀⠀⠀⠀⢸⡧⣿⣶⣨⠒⣁⠀⡇⣨⣿⠶⣤⣤⣿⠤⡤⡄⢸⢸⣇⣤⣤⡴⠚⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢃⠀
     ⠀⣀⣄⣰⠋⢠⣺⠞⠀⠀⠀⠀⣧⢼⣿⣀⠖⢹⡇⢸⢸⡝⠉⠉⢀⢔⣋⠹⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⢵⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡀
-    ⠀⢿⡀⠈⠳⣵⠁⡠⢄⠀⠀⢀⣸⣴⣇⠇⠀⠈⢧⣼⣾⣁⡀⠀⡈⡞⠛⣌⣀⣀⣀⣀⣀⣀⣀⣀⠀⢀⣀⣀⣀⣼⣃⣀⣀⣀⣀⣀⣀⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
-    ⢀⣼⡋⣶⣄⢈⡿⠀⠈⣷⠀⡏⣼⣿⣿⠀⠈⣿⣿⣿⣿⣿⡇⠀⡇⢸⣤⢿⣏⣉⣉⣉⣉⣉⣉⡉⣉⢉⠉⠉⠉⠉⠉⠉⠉⠉⠉⣹⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+    ⠀⢿⡀⠈⠳⣵⠁⡠⢄⠀⠀⢀⣸⣴⣇⠇⠀⠈⢧⣼⣾⣁⡀⠀⡈⡞⠛⣌⣀⣀⣀⣀⣀⣀⣀⠀⢀⣀⣀⣀⣼⣃⣀⣀⣀⣀⣀⣀⡗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
+    ⢀⣼⡋⣶⣄⢈⡿⠀⠈⣷⠀⡏⣼⣿⣿⠀⠈⣿⣿⣿⣿⣿⡇⠀⡇⢸⣤⢿⣏⣉⣉⣉⣉⣉⣉⡉⣉⢉⠉⠉⠉⠉⠉⠉⠉⠉⣹⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇
     ⢸⡆⠉⠉⠉⠁⢀⣠⣾⠏⠀⣿⣿⣿⣿⢠⠘⠛⡛⢛⣛⠛⠃⠀⠃⠀⣉⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣦⣖⣄⡀⡀⠀⠀⠀⠈⣇⠀⠀⠀⠀⠀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇
-    ⠀⠉⢲⠲⣖⠊⠉⠉⠀⠀⠀⣿⣿⣿⣿⡾⠀⢸⡇⢸⢸⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣶⣶⣾⣷⣲⣖⡖⢺⡽⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁
-    ⠀⠀⠈⢳⣽⣆⠀⠀⠀⠀⠀⣾⣿⣿⣿⣇⠀⠈⠳⠾⠞⠀⠀⢠⢰⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣋⣉⣷⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀
-    ⠀⠀⠀⠀⠙⢝⢢⡀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢛⣛⡭⠔⠒⠋⠉⠀⠀⠈⡅⠈⠓⠒⢿⡀⠀⠀⠀⠀⣠⠇⠀
-    ⠀⠀⠀⠀⠀⠀⠙⠻⢦⣄⣀⣀⣀⣉⣓⣭⣍⣉⡙⠛⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⠞⠛⠉⠀⠀⠀⠀⠀⠀⠀⢀⠀⡇⠀⠀⠀⠀⠙⠒⠓⠒⠚⠁⠀⠀
+    ⠀⠉⢲⠲⣖⠊⠉⠉⠀⠀⠀⣿⣿⣿⣿⡾⠀⢸⡇⢸⢸⠀⠀⠀⠀⠀⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣿⣶⣶⣾⣷⣲⣖⡖⢺⡽⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠁
+    ⠀⠀⠈⢳⣽⣆⠀⠀⠀⠀⠀⣾⣿⣿⣿⣇⠀⠈⠳⠾⠞⠀⠀⢠⢰⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣋⣉⣷⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀
+    ⠀⠀⠀⠀⠙⢝⢢⡀⠀⠀⠀⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢛⣛⡭⠔⠒⠋⠉⠀⠀⠈⡅⠈⠓⠒⢿⡀⠀⠀⠀⠀⣠⠇⠀
+    ⠀⠀⠀⠀⠀⠀⠙⠻⢦⣄⣀⣀⣀⣉⣓⣭⣍⣉⡙⠛⠛⠻⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡽⠞⠛⠉⠀⠀⠀⠀⠀⠀⠀⢀⠀⡇⠀⠀⠀⠀⠙⠒⠓⠒⠚⠁⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣦⣨⡭⠙⢛⣿⠿⣟⡛⠉⠉⠉⠉⠉⢹⠀⠀⠘⠛⠛⢻⣇⠀⠀⠀⠀⠀⠀⠀⠄⢲⣎⠀⠀⢻⠓⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣯⢾⠀⢠⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⡞⠀⠀⠀⠀⠀⠀⠹⣧⠀⠀⠀⠀⠀⠀⠀⠀⣻⠆⠀⠸⡇⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠒⠒⠓⠒⠒⠒⠒⠒⠒⠂⠀⠉⠁⠀⠀⠀⠀⠀⠀⠀⠛⠒⠲⠶⠦⠤⠤⠤⠤⠽⠭⠤⠼⠵⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-    Scanning images folder... 
+    Scanning images folder...
     `);
-    
+
+// Keep the upload functionality as is
+const uploadToSupabaseS3 = async (bucket, fileName, filePath) => {
+  try {
+    const fileStream = fs.createReadStream(filePath);
+    const contentType = getFallbackMimeType(fileName);
+
+    const params = {
+      Bucket: bucket,
+      Key: fileName,
+      Body: fileStream,
+      ContentType: contentType,
+    };
+
+    const command = new PutObjectCommand(params);
+    await s3Client.send(command);
+
+    console.log(`Uploaded to Supabase S3: ${fileName}`);
+    return `https://${process.env.SUPABASE_S3_ENDPOINT}/storage/v1/object/public/${bucket}/${fileName}?t=${encodeURIComponent(new Date().toISOString())}`;
+  } catch (err) {
+    console.error(`Failed to upload ${fileName} to Supabase S3:`, err);
+    return null;
+  }
+};
+
+// Hardcode the image URLs here, no pulling from Supabase S3
+const fetchImagesFromS3 = async (bucket) => {
+  return [
+    {
+      name: '1.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/1.jpg?t=2024-12-06T12%3A05%3A40.993Z'
+    },
+    {
+      name: '2.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/2.jpg'
+    },
+    {
+      name: '3.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/3.jpg?t=2024-12-06T12%3A06%3A23.593Z'
+    },
+    {
+      name: '4.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/4.jpg?t=2024-12-06T12%3A06%3A31.098Z'
+    },
+    {
+      name: '5.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/5.jpg?t=2024-12-06T12%3A06%3A43.675Z'
+    },
+    {
+      name: '6.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/6.jpg'
+    }
+  ];
+};
+
+const fetchImageByName = async (bucket, imageName) => {
+  const images = [
+    {
+      name: '1.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/1.jpg?t=2024-12-06T12%3A05%3A40.993Z'
+    },
+    {
+      name: '2.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/2.jpg'
+    },
+    {
+      name: '3.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/3.jpg?t=2024-12-06T12%3A06%3A23.593Z'
+    },
+    {
+      name: '4.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/4.jpg?t=2024-12-06T12%3A06%3A31.098Z'
+    },
+    {
+      name: '5.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/5.jpg?t=2024-12-06T12%3A06%3A43.675Z'
+    },
+    {
+      name: '6.jpg',
+      url: 'https://nshnjknplbtlaolcimgh.supabase.co/storage/v1/object/public/Images/6.jpg'
+    }
+  ];
+
+  const image = images.find((img) => img.name === imageName);
+  if (!image) {
+    throw new Error(`Image with name ${imageName} not found`);
+  }
+  return image;
+};
 
 const syncImages = async () => {
   try {
@@ -66,22 +158,6 @@ const syncImages = async () => {
     console.log('Image sync complete.');
   } catch (error) {
     console.error('Error syncing images:', error);
-  }
-};
-
-const fetchImageByName = async (bucket, imageName) => {
-  try {
-    const images = await fetchImagesFromS3(bucket);
-    const image = images.find((img) => img.name === imageName);
-
-    if (!image) {
-      throw new Error(`Image with name ${imageName} not found`);
-    }
-
-    return image;
-  } catch (error) {
-    console.error(`Error fetching image ${imageName}:`, error);
-    throw new Error('Failed to fetch image');
   }
 };
 
