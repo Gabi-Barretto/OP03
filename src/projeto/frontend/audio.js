cc164031c320b17e4b7410ac2d1051e05884b190
mateusalm <mateus.almeida@sou.inteli.edu.br>
2024-12-11 11:55:43 -0300

    adding audio script

---
+++ src/projeto/frontend/audio.js
@@ -0,0 +1,29 @@
+const audio = new Audio('../public/audio/jazz.mp3')
+var selectedGenre = 0
+audio.loop = true
+audio.play()
+var isPlaying = true
+document.querySelector('#mute-music-button').addEventListener('click', () => {
+  if (isPlaying) {
+    audio.pause()
+  } else {
+    audio.play()
+  }
+  isPlaying = !isPlaying
+})
+document.querySelector('#jazz-button').addEventListener('click', () => {
+  if (selectedGenre != 0) {
+    audio.src = '../public/audio/jazz.mp3'
+    audio.load()
+    audio.play()
+    selectedGenre = 0
+  }
+})
+document.querySelector('#electronic-button').addEventListener('click', () => {
+  if (selectedGenre != 1) {
+    audio.src = '../public/audio/house.mp3'
+    audio.load()
+    audio.play()
+    selectedGenre = 1
+  }
+})
