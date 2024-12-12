524a0a975d9f27cea14a2f6d2aae93395a6a88ad
Gabi-Barretto <gabriela.dias@sou.inteli.edu.br>
2024-12-12 10:01:21 -0300

    Deploy website - based on a7249856c553bcc74c8292f0d271546c9e7fe717

--- src/projeto/backend/utils/progressUtil.js
+++
@@ -1,9 +0,0 @@
-const cliProgress = require('cli-progress');
-
-const createProgressBar = (total) => {
-  const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
-  progressBar.start(total, 0);
-  return progressBar;
-};
-
-module.exports = { createProgressBar };
